3b05c1d5e53db7e550850e8bd259f79cef54c711
SFrasia <108404961+SFrasia@users.noreply.github.com>
2024-02-28 18:10:30 +0300

    Changes

---
+++ src/data/data.js
@@ -0,0 +1,7 @@
+// use these variables in your JSX (update them with your own name and city!)
+const name = "Frasia";
+const city = "Nairobi";
+
+const image = "https://i.imgur.com/mV8PQxj.gif";
+
+export { name, city, image };
